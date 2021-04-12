86be3750795d47b135df30371946dba0467bc590
aknakshay <34308903+aknakshay@users.noreply.github.com>
2021-04-12 16:04:24 +0530

    Add files via upload

---
+++ read-gps-serial.ino
@@ -0,0 +1,27 @@
+#include <SoftwareSerial.h>
+
+// Choose two Arduino pins to use for software serial
+int RXPin = 8;
+int TXPin = 7;
+
+//Default baud of NEO-6M is 9600
+int GPSBaud = 9600;
+
+// Create a software serial port called "gpsSerial"
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
+void setup()
+{
+  // Start the Arduino hardware serial port at 9600 baud
+  Serial.begin(GPSBaud);
+
+  // Start the software serial port at the GPS's default baud
+  gpsSerial.begin(GPSBaud);
+}
+
+void loop()
+{
+  // Displays information when new sentence is available.
+  while (gpsSerial.available() > 0)
+    Serial.write(gpsSerial.read());
+}
