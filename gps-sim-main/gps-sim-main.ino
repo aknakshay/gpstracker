cb391ccbf579e222e3f5d2f0fe1fdf1df6f7063a
Akshay Nagpal <akshaynagpal1995@gmail.com>
2021-04-14 01:32:37 +0530

    optimized

--- gps-sim-main/gps-sim-main.ino
+++ gps-sim-main/gps-sim-main.ino
@@ -1,15 +1,8 @@
-#include <Ftp.h>
-#include <Geo.h>
-#include <GPRS.h>
-#include <Http.h>
-#include <Parser.h>
-#include <Result.h>
-#include <Sim800.h>
+
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
 #include <String.h>
-
-
+#include <limits.h>
 
 // Choose two Arduino pins to use for software serial
 int RXPin = 12;
@@ -24,78 +17,20 @@ SoftwareSerial Sim800l(RXPinSIM,TXPinSIM);
 // Create a TinyGPS++ object
 TinyGPSPlus gps;
 
-String data;
-String latitude;
-String longitude;
-String alti;
-String curDate;
-String sat;
-String curTime;
+//String data;
+//String latitude;
+//String longitude;
+//String alti;
+//String curDate;
+//String sat;
+//String curTime;
 
-
-void setup() {
-  // put your setup code here, to run once:
- Serial.begin(9600);
- Sim800l.begin(9600);
- gpsSerial.begin(9600);
- 
- Serial.println("Start Program");
-
- simSetup();
-}
-
-
-void loop()
-{
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (gpsSerial.available() > 0)
-    if (gps.encode(gpsSerial.read()))
-      sendData(getGPSData());
-
-  // If 5000 milliseconds pass and there are no characters coming in
-  // over the software serial port, show a "No GPS detected" error
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-  {
-    Serial.println("No GPS detected");
-    while(true);
-  }
-
-    delay(10000);
-
-}
-
-
-void sendData(String data){
-
-  String datanew = String("{\"data\": ") + data + String("}");
-  Serial.println(datanew);
-  char * tab2 = new char [datanew.length()+1];
-  strcpy (tab2, datanew.c_str());
-  Serial.println("posting request\n\n");
-
-  Sim800l.write("AT+CIPSTART=\"TCP\",\"31.171.250.88\",8080");
-  Sim800l.write("WAIT=3");
-
-  String request = String("\nPOST / HTTP/1.1\nHost: 31.171.250.88:8080\nContent-Type: application/json\nContent-Length:") + static_cast<String>(datanew.length()) + String("\n");
-  Serial.println(request.length());
-  String frequest = String("CIPSEND=") + static_cast<String>(request.length()+11) + String(request);
-  Serial.println(request);
-  Serial.println(frequest);
-  Serial.println(tab2);
-  char * req = new char [frequest.length()+1];
-  strcpy (req, frequest.c_str());
-  
-  Sim800l.write(req);
-  Sim800l.write(tab2);
-
-  Serial.println("********************************");
-}
 
 void simSetup(){
 
   Serial.println("setting up your sim");
 //  enter GPRS configuration mode
-  Sim800l.write("AT+CGATT=1\r\n");
+  Sim800l.write("a\r\n");
 // Disable multi IP connection mode.  Single IP cnxn only
   Sim800l.write("AT+CIPMUX=0\r\n");
 // set up apn
@@ -113,54 +48,127 @@ void simSetup(){
   
   }
 
-void simSleepOff(){
+
+String getGPSData()
+{
+
+  static double latitude = LONG_MIN;
+  static double longitude = LONG_MIN;
+  static double alti = LONG_MIN;
+  
+  if (gps.location.isValid())
+  {
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+    alti = gps.altitude.meters();
+  }
+
+  static int date_month = INT_MIN;
+  static int date_day = INT_MIN;
+  static int date_year = INT_MIN;
+  
+  if (gps.date.isValid())
+  {
+  date_month = gps.date.month();
+  date_day = gps.date.day();
+  date_year = gps.date.year();
+  }
+
+
+  int sat = gps.satellites.value();
+  static int time_hour = INT_MIN;
+  static int time_minute = INT_MIN;
+  static int time_second = INT_MIN;
+  
+  if (gps.time.isValid())
+  {
+    time_hour = gps.time.hour();
+    time_minute = gps.time.minute();
+    time_second = gps.time.second();
+  }
+
+  
+
+  String data = String("{latitude:") + latitude + ",longitude" + longitude + ",altitude:" + alti + 
+  ",date_month:" + date_month + ",date_day:" + date_day + ",date_year:" + date_year  + ",satellites:" + sat + 
+  ",time_hour:" + time_hour + ",time_minute:" + time_minute + ",time_second:" + time_second +"}";
+
+  Serial.print("gps data concatenated");
+  return data;
+ }
+
+
+void sendData(String data){
+
+  String datanew("{\"data\": " + data + "}");
+    
+  Serial.println(datanew);
+  Serial.println("posting request\n\n");
+
+  Sim800l.write("AT+CIPSTART=\"TCP\",\"31.171.250.88\",8080");
+  Sim800l.write("WAIT=3");
+
+
+  String request("\nPOST / HTTP/1.1\nHost: 31.171.250.88:8080\nContent-Type: application/json\nContent-Length:" + datanew.length() + String("\n"));
+  Serial.println(request.length());
+
+  String frequest("CIPSEND=" + request.length() + request);
+  Serial.println(request);
+  Serial.println(frequest);
+
+  
+  Sim800l.write(frequest.c_str());
+
+  Serial.println("********************************");
+
+}
+
+
+
+void simSleepModeOff(){
   Sim800l.write("AT\r\n");
   Sim800l.write("AT+CSCLK=0'\r\n");
   delay(5000);
 }
 
-void simSleepMode(){
+void simSleepModeOn(){
   Sim800l.write("AT+CSCLK=2'\r\n");
   delay(5000);
 }
 
-String getGPSData()
+
+
+
+ 
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ Sim800l.begin(9600);
+ gpsSerial.begin(9600);
+ 
+ Serial.println("Start Program");
+
+ //simSetup();
+ 
+}
+
+
+void loop()
 {
-  if (gps.location.isValid())
-  {
-    latitude = static_cast<String>(gps.location.lat());
-    longitude = static_cast<String>(gps.location.lng());
-    alti = static_cast<String>(gps.altitude.meters());
-  }
-  else
-  {
-    latitude = "NA";
-    longitude = "NA";
-    alti = "NA";
-  }
-  
-  if (gps.date.isValid())
-  {
-    curDate = static_cast<String>(gps.date.month())  +  "/"  + static_cast<String>(gps.date.day()) + "/" + static_cast<String>(gps.date.year());
-  }
-  else
-  {
-    curDate = "NA" ;
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (gpsSerial.available() > 0){
+    if (gps.encode(gpsSerial.read()))
+      Serial.println(getGPSData());
   }
 
-  sat = static_cast<String>(gps.satellites.value());
-  
-  if (gps.time.isValid())
+  // If 5000 milliseconds pass and there are no characters coming in
+  // over the software serial port, show a "No GPS detected" error
+  if (millis() > 5000 && gps.charsProcessed() < 10)
   {
-    curTime = static_cast<String>(gps.time.hour()) + ":" + static_cast<String>(gps.time.minute()) + ":" + static_cast<String>(gps.time.second());
-  }
-  else
-  {
-    curTime = "NA";
+    Serial.println("No GPS detected");
+    while(true);
   }
 
-  String data = String("{latitude:") + latitude + ",longitude" + longitude + ",altitude:" + alti + ",date:" + curDate + ",satellites:" + sat + ",time:" + curTime + "}";
+    delay(10000);
 
-  Serial.print("gps data concatenated");
-  return data;
- }
+}
